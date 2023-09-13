08006a8e9f569b7e658f1513696229d4ef05223b
Emanuel Pecson <emanpecson@gmail.com>
2023-09-13 14:56:09 -0700

    style: :lipstick: test home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
 	return (
-		<div>test</div>
+		<div className="h-screen w-screen text-3xl justify-center place-items-center flex ">HIIIII JESSSSSSSSS</div>
 	);
 }
