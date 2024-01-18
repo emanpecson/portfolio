6fab90036cb5d09c67a973a8b38a27a003e47f53
Emanuel Pecson <emanpecson@gmail.com>
2024-01-17 20:58:11 -0800

    style: :lipstick: update ui homepage to temporary "work in progress" state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,15 @@
-"use client";
-
-import { useState } from "react";
-
 export default function Home() {
-	const [toggle, setToggle] = useState(true);
-
-	return (
-		<>
-			<div className="h-screen w-screen text-3xl justify-center place-items-center flex flex-col bg-green-300 text-green-800">
-				{toggle && (
-					<p>
-						hi jess you are a very special person to me and i hope you are doing
-						well {":]"}
-					</p>
-				)}
-				{!toggle && (
-					<p className="font-bold">SIKE U SUCK AT BOWLING GET GOOOOOOOOD</p>
-				)}
-				<button
-					className="rounded-lg bg-gray-700 hover:bg-gray-800 text-gray-200 px-3 py-2 mt-8 text-lg"
-					onClick={() => setToggle(!toggle)}
-				>
-					press this button!!
-				</button>
-			</div>
-		</>
-	);
+  return (
+    <div className="h-screen w-screen flex justify-center place-items-center">
+      <div className="text-center">
+        <p>work in progress</p>
+        <div className="flex space-x-1">
+          <p>check out:</p>
+          <p className="text-blue-400 hover:text-blue-600 transition-colors duration-200 hover:cursor-pointer">
+            https://github.com/emanpecson
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
