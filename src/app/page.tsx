d1dbcb9309f3e3d4b568b1083cb15a66277c1736
Emanuel Pecson <emanpecson@gmail.com>
2024-01-17 22:09:43 -0800

    refactor: :recycle: route to github on click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div className="h-screen w-screen flex justify-center place-items-center">
       <div className="text-center">
         <p>work in progress</p>
         <div className="flex space-x-1">
           <p>check out:</p>
-          <p className="text-blue-400 hover:text-blue-600 transition-colors duration-200 hover:cursor-pointer">
+          <button
+            className="text-blue-400 hover:text-blue-600 transition-colors duration-200 hover:cursor-pointer"
+            onClick={() => router.push('https://github.com/emanpecson')}>
             https://github.com/emanpecson
-          </p>
+          </button>
         </div>
       </div>
     </div>
