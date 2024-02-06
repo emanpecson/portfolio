c11594386db9505c62bf1ec86538aa114f41262b
Emanuel Pecson <emanpecson@gmail.com>
2024-02-05 22:05:34 -0800

    style: :lipstick: use Inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Roboto } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
-const roboto = Roboto({
+const inter = Inter({
   subsets: ['latin'],
-  weight: '400',
+  // weight: '400',
 });
 
 export const metadata: Metadata = {
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
