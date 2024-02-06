af64b1dd12726c7bea09285e019845afe4d3cce5
Emanuel Pecson <emanpecson@gmail.com>
2024-02-05 21:29:10 -0800

    style: :lipstick: setup layout for profile/content sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,14 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-
 export default function Home() {
-  const router = useRouter();
-
   return (
-    <div className="h-screen w-screen flex justify-center place-items-center">
-      <div className="text-center">
-        <p>work in progress</p>
-        <div className="flex space-x-1">
-          <p>check out:</p>
-          <button
-            className="text-blue-400 hover:text-blue-600 transition-colors duration-200 hover:cursor-pointer"
-            onClick={() => router.push('https://github.com/emanpecson')}>
-            https://github.com/emanpecson
-          </button>
+    <>
+      <div className="xl:grid xl:grid-cols-10 h-screen pt-20">
+        <div className="xl:col-span-4 bg-red-500 xl:h-full h-[24rem]">
+          <div className="mx-20 bg-blue-300">profile content</div>
+        </div>
+        <div className="xl:col-span-6 bg-blue-500 h-full">
+          <div className="mx-20 bg-green-300">desc content</div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
