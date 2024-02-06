fca8d0f138dc0e2e48558e0ca7d419d2dfd4f15e
Emanuel Pecson <emanpecson@gmail.com>
2024-02-05 22:06:08 -0800

    style: :lipstick: update format to support fixed profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 export default function Home() {
   return (
     <>
-      <div className="xl:grid xl:grid-cols-10 h-screen pt-20">
-        <div className="xl:col-span-4 bg-red-500 xl:h-full h-[24rem]">
-          <div className="mx-20 bg-blue-300">profile content</div>
+      <div className="xl:grid xl:grid-cols-12 h-screen pt-20">
+        <div className="xl:col-span-5 bg-red-500 xl:h-full h-[24rem]">
+          <div className="mx-16 bg-blue-300 fixed">
+            <div>
+              <h1 className="text-5xl font-bold">Emanuel Pecson</h1>
+              <h2 className="text-2xl pt-2 font-semibold">Software Engineer Intern at JT4</h2>
+            </div>
+          </div>
         </div>
-        <div className="xl:col-span-6 bg-blue-500 h-full">
-          <div className="mx-20 bg-green-300">desc content</div>
+        <div className="xl:col-span-7 bg-blue-500 h-full">
+          <div className="mx-16 bg-green-300">
+            <div className="bg-orange-500 h-[20rem] my-2">block</div>
+            <div className="bg-orange-500 h-[20rem] my-2">block</div>
+            <div className="bg-orange-500 h-[20rem] my-2">block</div>
+            <div className="bg-orange-500 h-[20rem] my-2">block</div>
+          </div>
         </div>
       </div>
     </>
