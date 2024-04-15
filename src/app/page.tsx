138b8ea11c5be191c15f02134ee49a4df24acba3
Emanuel Pecson <emanpecson@gmail.com>
2024-04-15 00:11:26 -0700

    fix: :bug: specify fixed position for lg screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
     <>
       <div className="lg:grid lg:grid-cols-12 h-full py-20 mx-auto max-w-[100rem]">
         <div className="lg:col-span-5 lg:h-full h-[24rem] flex justify-center">
-          <div className="mx-6 sm:mx-16 h-full fixed max-w-xl">
+          <div className="mx-6 sm:mx-16 h-full lg:fixed max-w-xl">
             <div className="flex flex-col place-content-between h-4/5">
               <div>
                 <Heading />
