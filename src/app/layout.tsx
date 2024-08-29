a35743023878a7503afec5401d9239e460c260dd
Emanuel Pecson <emanpecson@gmail.com>
2024-08-28 19:03:39 -0700

    feat: :sparkles: install shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
+import { cn } from '@/lib/utils';
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+// import { Inter } from 'next/font/google';
+import { Inter as FontSans } from 'next/font/google';
 
-const inter = Inter({
+const fontSans = FontSans({
   subsets: ['latin'],
-  // weight: '400',
+  variable: '--font-sans',
 });
 
 export const metadata: Metadata = {
@@ -15,7 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>{children}</body>
     </html>
   );
 }
