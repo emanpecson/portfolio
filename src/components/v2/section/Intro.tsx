e7acb43b55363b3b6022e55290aeccee44bd2002
Emanuel Pecson <emanpecson@gmail.com>
2024-08-30 23:12:43 -0700

    feat: :sparkles: add profile picture

--- src/components/v2/section/Intro.tsx
+++ src/components/v2/section/Intro.tsx
@@ -1,13 +1,24 @@
 import { greeting, mission, position } from '@/data/intro';
+import Image from 'next/image';
 
 export default function Intro() {
   return (
-    <div className="font-medium space-y-4">
-      <div>
-        <h1 className="text-6xl">{greeting}</h1>
-        <p className="text-2xl">{position}</p>
+    <div className="flex space-x-8">
+      <div className="font-medium space-y-4">
+        <div>
+          <h1 className="text-6xl">{greeting}</h1>
+          <p className="text-2xl">{position}</p>
+        </div>
+        <p className="text-2xl">{mission}</p>
       </div>
-      <p className="text-2xl">{mission}</p>
+      <Image
+        src="/linkedin_pfp.jpeg"
+        alt="linkedin_pfp"
+        width="24"
+        height="24"
+        className="h-36 w-36 rounded-full"
+        unoptimized
+      />
     </div>
   );
 }
