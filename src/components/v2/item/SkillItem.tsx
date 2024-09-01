15c1f0a3604eca6253d75665130f30b8b42ec40a
Emanuel Pecson <emanpecson@gmail.com>
2024-09-01 16:50:58 -0700

    style: :lipstick: decrease skill-item icon size

--- src/components/v2/item/SkillItem.tsx
+++ src/components/v2/item/SkillItem.tsx
@@ -10,7 +10,7 @@ export default function SkillItem({ skill }: { skill: SkillType }) {
       href={skill.url}
       className="cursor-pointer rounded-full py-1 flex border dark:border-neutral-800 border-neutral-300 w-fit place-items-center space-x-3 pl-1.5 dark:bg-neutral-900 bg-neutral-50 dark:hover:bg-neutral-900/70 dark:hover:text-white text-neutral-500 duration-200 transition-colors hover:bg-neutral-200/70 hover:text-neutral-700"
     >
-      <div className="bg-white rounded-full h-8 w-8 relative flex border border-neutral-300 dark:border-none">
+      <div className="bg-white rounded-full h-6 w-6 relative flex border border-neutral-300 dark:border-none">
         <Image
           src={skill.iconSrc}
           alt={skill.iconSrc}
