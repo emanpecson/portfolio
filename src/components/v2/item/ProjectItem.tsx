bd08a5808fe20d0fd1289a39c778178bf79e31eb
Emanuel Pecson <emanpecson@gmail.com>
2024-09-01 18:16:12 -0700

    style: :lipstick: swap repo/website button

--- src/components/v2/item/ProjectItem.tsx
+++ src/components/v2/item/ProjectItem.tsx
@@ -31,12 +31,6 @@ export default function ProjectItem({ proj }: { proj: ProjectType }) {
             {proj.tags.length > 0 && proj.tags.map((tag: SkillType, i: number) => <SkillItem skill={tag} />)}
           </div>
           <div className="flex space-x-2">
-            <Link rel="noopener noreferrer" target="_blank" href={proj.repoUrl}>
-              <Button size="sm" className="flex space-x-1.5 duration-150">
-                <GithubIcon size={20} />
-                <p className="font-semibold">Repository</p>
-              </Button>
-            </Link>
             {!!proj.websiteUrl && (
               <Link rel="noopener noreferrer" target="_blank" href={proj.websiteUrl}>
                 <Button size="sm" className="flex space-x-1.5 duration-150">
@@ -45,6 +39,12 @@ export default function ProjectItem({ proj }: { proj: ProjectType }) {
                 </Button>
               </Link>
             )}
+            <Link rel="noopener noreferrer" target="_blank" href={proj.repoUrl}>
+              <Button size="sm" className="flex space-x-1.5 duration-150">
+                <GithubIcon size={20} />
+                <p className="font-semibold">Repository</p>
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
