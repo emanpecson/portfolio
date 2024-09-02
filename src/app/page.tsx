a953a93bad771f6d89ee4cbc42807843c0e89618
Emanuel Pecson <emanpecson@gmail.com>
2024-09-01 20:51:22 -0700

    refactor: :recycle: set main page as v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,23 @@
-'use client';
-
-import { useRef } from 'react';
-import BackgroundSection from '@/components/v1/main/section/BackgroundSection';
-import ExperienceSection from '@/components/v1/main/section/ExperienceSection';
-import ProjectSection from '@/components/v1/main/section/ProjectSection';
-import ContactMe from '@/components/v1/profile/ContactMe';
-import SectionNavigation from '@/components/v1/profile/SectionNavigation';
-import Heading from '@/components/v1/profile/Heading';
-import CursorEffect from '@/components/v1/CursorEffect';
+import FloatingDock from '@/components/v2/FloatingDock';
+import About from '@/components/v2/section/About';
+import Education from '@/components/v2/section/Education';
+import Experience from '@/components/v2/section/Experience';
+import Intro from '@/components/v2/section/Intro';
+import Projects from '@/components/v2/section/Projects';
+import Skills from '@/components/v2/section/Skills';
 
 export default function Home() {
-  const backgroundRef = useRef(null);
-  const experienceRef = useRef(null);
-  const projectRef = useRef(null);
-
   return (
-    <>
-      <div className="lg:grid lg:grid-cols-12 h-full py-20 mx-auto max-w-[100rem]">
-        <div className="lg:col-span-5 lg:h-full h-[24rem] flex justify-center">
-          <div className="mx-6 sm:mx-16 h-full lg:fixed max-w-xl">
-            <div className="flex flex-col place-content-between h-4/5">
-              <div>
-                <Heading />
-                <SectionNavigation params={{ backgroundRef, experienceRef, projectRef }} />
-              </div>
-              <ContactMe />
-            </div>
-          </div>
-        </div>
-
-        <div className="lg:col-span-7 h-full">
-          <div className="sm:mx-16 space-y-20">
-            <div ref={backgroundRef}>
-              <BackgroundSection />
-            </div>
-
-            <div ref={experienceRef}>
-              <div className="sm:hidden block h-14 sticky top-0 w-full bg-app bg-opacity-50 backdrop-blur-sm">
-                <p className="flex place-items-center h-full text-gray-300 font-light px-6">EXPERIENCE</p>
-              </div>
-
-              <ExperienceSection />
-            </div>
-
-            <div ref={projectRef}>
-              <div className="sm:hidden block h-14 sticky top-0 w-full bg-app bg-opacity-50 backdrop-blur-sm">
-                <p className="flex place-items-center h-full text-gray-300 font-light px-6">PROJECTS</p>
-              </div>
-
-              <ProjectSection />
-            </div>
-          </div>
-        </div>
+    <div className="flex justify-center py-24 px-4">
+      <FloatingDock />
+      <div className="w-[56rem] space-y-10">
+        <Intro />
+        <About />
+        <Experience />
+        <Education />
+        <Skills />
+        <Projects />
       </div>
-
-      <CursorEffect />
-    </>
+    </div>
   );
 }
