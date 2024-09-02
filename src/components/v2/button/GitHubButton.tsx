471b140a15f57e1c697316f8e3b28c4c1c71c8b0
Emanuel Pecson <emanpecson@gmail.com>
2024-09-01 20:45:03 -0700

    style: :lipstick: set buttons as fitted

--- src/components/v2/button/GitHubButton.tsx
+++ src/components/v2/button/GitHubButton.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function GitHubButton({ href }: { href: string }) {
   return (
     <Button size="sm" className="duration-150" asChild>
-      <Link rel="noopener noreferrer" target="_blank" href={href} className="flex space-x-1.5 w-full">
+      <Link rel="noopener noreferrer" target="_blank" href={href} className="flex space-x-1.5 w-fit">
         <GithubIcon size={20} />
         <p className="font-semibold">Repository</p>
       </Link>
--- src/components/v2/button/WebsiteButton.tsx
+++ src/components/v2/button/WebsiteButton.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function WebsiteButton({ href }: { href: string }) {
   return (
     <Button size="sm" className="duration-150" asChild>
-      <Link rel="noopener noreferrer" target="_blank" href={href} className="flex space-x-1.5">
+      <Link rel="noopener noreferrer" target="_blank" href={href} className="flex space-x-1.5 w-fit">
         <GlobeIcon size={20} />
         <p className="font-semibold">Website</p>
       </Link>
