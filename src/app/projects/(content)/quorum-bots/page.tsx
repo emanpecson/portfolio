2081ceb025daf79dcf15ab4632d9488e28935466
Emanuel Pecson <emanpecson@gmail.com>
2024-12-31 06:21:05 -0800

    style: :lipstick: add "impact" heading to quorum-bots

--- src/app/projects/(content)/quorum-bots/page.tsx
+++ src/app/projects/(content)/quorum-bots/page.tsx
@@ -10,18 +10,19 @@ import ContentSubsection from '@/components/content/ContentSubsection';
 
 export default function QuorumBotsPage() {
   const proj = projects[2];
-  const refMap = useRefMap(['problem', 'solution', 'features', 'f1', 'f2', 'f3']);
+  const refMap = useRefMap(['problem', 'solution', 'impact', 'features', 'f1', 'f2', 'f3']);
   const headers: ContentHeaderType[] = [
     { id: '0', label: 'Problem', ref: refMap.problem },
     { id: '1', label: 'Solution', ref: refMap.solution },
+    { id: '2', label: 'Impact', ref: refMap.impact },
     {
-      id: '2',
+      id: '3',
       label: 'Features',
       ref: refMap.features,
       subheaders: [
-        { id: '2.0', label: 'Feature 1', ref: refMap.f1 },
-        { id: '2.1', label: 'Feature 2', ref: refMap.f2 },
-        { id: '2.2', label: 'Feature 3', ref: refMap.f3 },
+        { id: '3.0', label: 'Feature 1', ref: refMap.f1 },
+        { id: '3.1', label: 'Feature 2', ref: refMap.f2 },
+        { id: '3.2', label: 'Feature 3', ref: refMap.f3 },
       ],
     },
   ];
@@ -43,17 +44,20 @@ export default function QuorumBotsPage() {
         </ContentText>
       </ContentSection>
       <ContentSection header={headers[2]}>
-        <ContentSubsection subheader={headers[2].subheaders![0]}>
+        <ContentText>This is an impact project because it ...</ContentText>
+      </ContentSection>
+      <ContentSection header={headers[3]}>
+        <ContentSubsection subheader={headers[3].subheaders![0]}>
           proident laborum sit amet do deserunt nisi ullamco nostrud aute dolore officia mollit sint esse amet aliquip
           occaecat consectetur ea amet qui eiusmod amet veniam excepteur dolor sit duis reprehenderit do minim dolore
           sunt deserunt est nostrud elit aute commodo
         </ContentSubsection>
-        <ContentSubsection subheader={headers[2].subheaders![1]}>
+        <ContentSubsection subheader={headers[3].subheaders![1]}>
           proident laborum sit amet do deserunt nisi ullamco nostrud aute dolore officia mollit sint esse amet aliquip
           occaecat consectetur ea amet qui eiusmod amet veniam excepteur dolor sit duis reprehenderit do minim dolore
           sunt deserunt est nostrud elit aute commodo
         </ContentSubsection>
-        <ContentSubsection subheader={headers[2].subheaders![2]}>
+        <ContentSubsection subheader={headers[3].subheaders![2]}>
           proident laborum sit amet do deserunt nisi ullamco nostrud aute dolore officia mollit sint esse amet aliquip
           occaecat consectetur ea amet qui eiusmod amet veniam excepteur dolor sit duis reprehenderit do minim dolore
           sunt deserunt est nostrud elit aute commodo
