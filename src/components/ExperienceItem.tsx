68f1d58f310bb4e67bfc506e4fdea0310d60a428
Emanuel Pecson <emanpecson@gmail.com>
2025-02-22 15:41:31 -0800

    refactor: :recycle: modify experiences to include multiple positions

--- src/components/ExperienceItem.tsx
+++ src/components/ExperienceItem.tsx
@@ -1,4 +1,4 @@
-import { ExperienceType } from '@/types/ExperienceType';
+import { ExperienceType, PositionType } from '@/types/ExperienceType';
 import { TaskType } from '@/types/TaskType';
 import Link from 'next/link';
 import TaskItem from './TaskItem';
@@ -8,54 +8,58 @@ import BlurFade from './ui/blur-fade';
 
 export default function ExperienceItem({ experience }: { experience: ExperienceType }) {
   return (
-    <div className="space-y-4">
-      <BlurFade delay={0}>
-        <Link href={experience.website} target="_blank" rel="noopener noreferrer">
-          <BoxWrap>
-            <div className="flex space-x-4 place-items-center">
-              <Image
-                src={experience.logoSrc}
-                alt={experience.company}
-                height={24}
-                width={24}
-                className="h-16 w-16 rounded-md object-contain"
-                unoptimized
-              />
-              <div>
-                <h2 className="font-extrabold sm:text-2xl text-lg">{experience.position}</h2>
-                <div className="sm:flex sm:space-x-4 sm:place-items-center">
-                  <p className="sm:font-semibold">{experience.timeline}</p>
-                  <div className="sm:block hidden rounded-full h-[5px] w-[5px] bg-neutral-800 dark:bg-neutral-200" />
-                  <p className="sm:font-semibold">{experience.location}</p>
+    <div className="divide-y">
+      {experience.positions.map((position: PositionType) => (
+        <div className="space-y-4 py-4">
+          <BlurFade delay={0}>
+            <Link href={experience.website} target="_blank" rel="noopener noreferrer">
+              <BoxWrap>
+                <div className="flex space-x-4 place-items-center">
+                  <Image
+                    src={experience.logoSrc}
+                    alt={experience.company}
+                    height={24}
+                    width={24}
+                    className="h-16 w-16 rounded-md object-contain"
+                    unoptimized
+                  />
+                  <div>
+                    <h2 className="font-extrabold sm:text-2xl text-lg">{position.name}</h2>
+                    <div className="sm:flex sm:space-x-4 sm:place-items-center">
+                      <p className="sm:font-semibold">{position.timeline}</p>
+                      <div className="sm:block hidden rounded-full h-[5px] w-[5px] bg-neutral-800 dark:bg-neutral-200" />
+                      <p className="sm:font-semibold">{position.location}</p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </BoxWrap>
-        </Link>
-      </BlurFade>
+              </BoxWrap>
+            </Link>
+          </BlurFade>
 
-      <div className="space-y-4">
-        {experience.tasks.map((task: TaskType, i: number) => {
-          if (task.externalLink) {
-            return (
-              <BlurFade delay={0.1 * i}>
-                <BoxWrap>
-                  <Link href={task.externalLink} className="hover:cursor-pointer">
+          <div className="space-y-3">
+            {position.tasks.map((task: TaskType, i: number) => {
+              if (task.externalLink) {
+                return (
+                  <BlurFade delay={0.1 * i}>
+                    <BoxWrap>
+                      <Link href={task.externalLink} className="hover:cursor-pointer">
+                        <TaskItem task={task} key={i} />
+                      </Link>
+                    </BoxWrap>
+                  </BlurFade>
+                );
+              }
+              return (
+                <BlurFade delay={0.1 * i}>
+                  <div className="p-2.5">
                     <TaskItem task={task} key={i} />
-                  </Link>
-                </BoxWrap>
-              </BlurFade>
-            );
-          }
-          return (
-            <BlurFade delay={0.1 * i}>
-              <div className="p-2.5">
-                <TaskItem task={task} key={i} />
-              </div>
-            </BlurFade>
-          );
-        })}
-      </div>
+                  </div>
+                </BlurFade>
+              );
+            })}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/data/experience.ts
+++ src/data/experience.ts
@@ -5,134 +5,158 @@ import { SkillType } from '@/types/SkillType';
 export const experiences: ExperienceType[] = [
   {
     company: 'JT4',
-    position: 'Software Engineer Intern',
-    location: 'Las Vegas, NV',
     logoSrc: '/logo/jt4.png',
     path: '/experience/jt4',
-    timeline: '05/2023 - Present',
     website: 'https://www.jt4llc.com',
-    tasks: [
+    positions: [
       {
-        header: 'Employee Image App',
-        description: 'Built a web app that allows users to update their profile picture among all corporate apps.',
-        label: 'full-stack webapp',
-        externalLink: '/projects/employee-image',
-        tags: [
-          overallSkills.get('Next') as SkillType,
-          overallSkills.get('React.js') as SkillType,
-          overallSkills.get('Node.js') as SkillType,
-          overallSkills.get('Tailwind CSS') as SkillType,
-          overallSkills.get('SQL Server') as SkillType,
-          overallSkills.get('Prisma') as SkillType,
-          overallSkills.get('Git') as SkillType,
+        name: 'Programmer I',
+        timeline: '02/2025 - Present',
+        location: 'Las Vegas, NV',
+        tasks: [
+          {
+            header: 'Employee Image App',
+            description: 'Built a web app that allows users to update their profile picture among all corporate apps.',
+            label: 'full-stack webapp',
+            externalLink: '/projects/employee-image',
+            tags: [
+              overallSkills.get('Next') as SkillType,
+              overallSkills.get('React.js') as SkillType,
+              overallSkills.get('Node.js') as SkillType,
+              overallSkills.get('Tailwind CSS') as SkillType,
+              overallSkills.get('SQL Server') as SkillType,
+              overallSkills.get('Prisma') as SkillType,
+              overallSkills.get('Git') as SkillType,
+            ],
+          },
         ],
       },
       {
-        header: 'Fileship',
-        description:
-          'Built a web application tool that provides employees with a way to securely upload files. This includes functionalities for uploading files, an automated email notification system, and enabling real-time features with a web-socket.',
-        label: 'full-stack webapp',
-        externalLink: '/projects/fileship',
-        tags: [
-          overallSkills.get('Nuxt') as SkillType,
-          overallSkills.get('Vue.js') as SkillType,
-          overallSkills.get('Node.js') as SkillType,
-          overallSkills.get('Tailwind CSS') as SkillType,
-          overallSkills.get('Docker') as SkillType,
-          overallSkills.get('Socket.IO') as SkillType,
-          overallSkills.get('Nodemailer') as SkillType,
-          overallSkills.get('SQL Server') as SkillType,
-          overallSkills.get('Prisma') as SkillType,
-          overallSkills.get('Git') as SkillType,
+        name: 'Software Engineer Intern',
+        timeline: '05/2023 - 02/2025',
+        location: 'Las Vegas, NV',
+        tasks: [
+          {
+            header: 'Fileship',
+            description:
+              'Built a web application tool that provides employees with a way to securely upload files. This includes functionalities for uploading files, an automated email notification system, and enabling real-time features with a web-socket.',
+            label: 'full-stack webapp',
+            externalLink: '/projects/fileship',
+            tags: [
+              overallSkills.get('Nuxt') as SkillType,
+              overallSkills.get('Vue.js') as SkillType,
+              overallSkills.get('Node.js') as SkillType,
+              overallSkills.get('Tailwind CSS') as SkillType,
+              overallSkills.get('Docker') as SkillType,
+              overallSkills.get('Socket.IO') as SkillType,
+              overallSkills.get('Nodemailer') as SkillType,
+              overallSkills.get('SQL Server') as SkillType,
+              overallSkills.get('Prisma') as SkillType,
+              overallSkills.get('Git') as SkillType,
+            ],
+          },
+          {
+            header: 'Concur Travel Approver',
+            description:
+              'Built a web application tool that simplifies the management of employee travel info. This app displays an employee table with options for managing their info, search and filter tools to quickly find employees, and support for updating employees over entire departments within a few clicks.',
+            label: 'full-stack webapp',
+            externalLink: '/projects/concur-travel-approver',
+            tags: [
+              overallSkills.get('Nuxt') as SkillType,
+              overallSkills.get('Vue.js') as SkillType,
+              overallSkills.get('Node.js') as SkillType,
+              overallSkills.get('Tailwind CSS') as SkillType,
+              overallSkills.get('Docker') as SkillType,
+              overallSkills.get('Prisma') as SkillType,
+              overallSkills.get('Git') as SkillType,
+              overallSkills.get('SQL Server') as SkillType,
+            ],
+          },
+          {
+            header: 'Automatic Birthday Mail',
+            description:
+              'Develop a SQL trigger that automatically emails employees a special message on their birthday.',
+            tags: [overallSkills.get('SQL') as SkillType, overallSkills.get('SQL Server') as SkillType],
+          },
         ],
-      },
-      {
-        header: 'Concur Travel Approver',
-        description:
-          'Built a web application tool that simplifies the management of employee travel info. This app displays an employee table with options for managing their info, search and filter tools to quickly find employees, and support for updating employees over entire departments within a few clicks.',
-        label: 'full-stack webapp',
-        externalLink: '/projects/concur-travel-approver',
-        tags: [
-          overallSkills.get('Nuxt') as SkillType,
-          overallSkills.get('Vue.js') as SkillType,
-          overallSkills.get('Node.js') as SkillType,
-          overallSkills.get('Tailwind CSS') as SkillType,
-          overallSkills.get('Docker') as SkillType,
-          overallSkills.get('Prisma') as SkillType,
-          overallSkills.get('Git') as SkillType,
-          overallSkills.get('SQL Server') as SkillType,
-        ],
-      },
-      {
-        header: 'Automatic Birthday Mail',
-        description: 'Develop a SQL trigger that automatically emails employees a special message on their birthday.',
-        tags: [overallSkills.get('SQL') as SkillType, overallSkills.get('SQL Server') as SkillType],
       },
     ],
   },
   {
     company: 'General Atomics',
-    position: 'Software Developer Intern',
-    location: 'San Diego, CA',
     logoSrc: '/logo/general_atomics.png',
     path: '/experience/general-atomics',
-    timeline: '06/2022 - 08/2022',
     website: 'https://www.ga.com',
-    tasks: [
+    positions: [
       {
-        header: 'Improved developer efficiency',
-        description:
-          'My team faced an issue where they relied on a 3rd party library and needed to send requests to them whenever we needed to update anything in the data model. So I worked up a solution where we would be able to generate the data model locally, so when we needed to make an update, we had direct access. As a result, update processes reduced from ~3 days to just ~30 minutes.',
-        tags: [overallSkills.get('CMake') as SkillType, overallSkills.get('Subversion') as SkillType],
-      },
-      {
-        header: 'Refactor existing code',
-        description:
-          'Updated an outdated Python script to perform the function of generating thousands of C++ class header files.',
-        tags: [overallSkills.get('Python') as SkillType],
-      },
-      {
-        header: 'Write unit tests',
-        description:
-          'Wrote basic unit tests with the GoogleTest framework to ensure the functionality of C++ source code.',
-        tags: [overallSkills.get('C++') as SkillType, overallSkills.get('GoogleTest') as SkillType],
+        name: 'Software Developer Intern',
+        timeline: '06/2022 - 08/2022',
+        location: 'San Diego, CA',
+        tasks: [
+          {
+            header: 'Improved developer efficiency',
+            description:
+              'My team faced an issue where they relied on a 3rd party library and needed to send requests to them whenever we needed to update anything in the data model. So I worked up a solution where we would be able to generate the data model locally, so when we needed to make an update, we had direct access. As a result, update processes reduced from ~3 days to just ~30 minutes.',
+            tags: [overallSkills.get('CMake') as SkillType, overallSkills.get('Subversion') as SkillType],
+          },
+          {
+            header: 'Refactor existing code',
+            description:
+              'Updated an outdated Python script to perform the function of generating thousands of C++ class header files.',
+            tags: [overallSkills.get('Python') as SkillType],
+          },
+          {
+            header: 'Write unit tests',
+            description:
+              'Wrote basic unit tests with the GoogleTest framework to ensure the functionality of C++ source code.',
+            tags: [overallSkills.get('C++') as SkillType, overallSkills.get('GoogleTest') as SkillType],
+          },
+        ],
       },
     ],
   },
   {
     company: 'Clark County',
-    position: 'Geographic Information System Intern',
-    location: 'Las Vegas, NV',
     logoSrc: '/logo/clark_county.png',
     path: '/experience/clark-county',
-    timeline: '01/2022 - 05/2022',
     website: 'https://www.clarkcountynv.gov',
-    tasks: [
+    positions: [
       {
-        header: 'Web-scraping',
-        description:
-          'Applied prior knowledge of programming to automate a manual process of storing office data into an ArcGIS data table using Python and Selenium.',
-        tags: [
-          overallSkills.get('Python') as SkillType,
-          overallSkills.get('Selenium') as SkillType,
-          overallSkills.get('ArcGIS') as SkillType,
+        name: 'Geographic Information System Intern',
+        timeline: '01/2022 - 05/2022',
+        location: 'Las Vegas, NV',
+        tasks: [
+          {
+            header: 'Web-scraping',
+            description:
+              'Applied prior knowledge of programming to automate a manual process of storing office data into an ArcGIS data table using Python and Selenium.',
+            tags: [
+              overallSkills.get('Python') as SkillType,
+              overallSkills.get('Selenium') as SkillType,
+              overallSkills.get('ArcGIS') as SkillType,
+            ],
+          },
         ],
       },
     ],
   },
   {
     company: 'UNLV',
-    position: 'Computer Science Teaching Assistant',
-    location: 'Las Vegas, NV',
     logoSrc: '/logo/unlv.png',
     path: '/experience/unlv',
-    timeline: '01/2022 - 05/2022',
     website: 'https://www.unlv.edu/cs',
-    tasks: [
+    positions: [
       {
-        header: 'Tutoring',
-        description: 'Assisted college students in learning introductory computer science concepts in C++.',
-        tags: [overallSkills.get('C++') as SkillType],
+        name: 'Computer Science Teaching Assistant',
+        timeline: '01/2022 - 05/2022',
+        location: 'Las Vegas, NV',
+        tasks: [
+          {
+            header: 'Tutoring',
+            description: 'Assisted college students in learning introductory computer science concepts in C++.',
+            tags: [overallSkills.get('C++') as SkillType],
+          },
+        ],
       },
     ],
   },
