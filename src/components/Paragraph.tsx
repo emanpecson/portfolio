332022c82e6dab0cfd7516f3e91fbe2cea74fa97
Emanuel Pecson <emanpecson@gmail.com>
2025-06-07 05:02:38 -0700

    fix: :bug: prevent div tags within p tag

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -3,5 +3,7 @@ interface ParagraphProps {
 }
 
 export default function Paragraph(props: ParagraphProps) {
-  return <p className="text-base text-neutral-800/80 dark:text-neutral-200/80 text-wrap leading-7">{props.children}</p>;
+  return (
+    <div className="text-base text-neutral-800/80 dark:text-neutral-200/80 text-wrap leading-7">{props.children}</div>
+  );
 }
