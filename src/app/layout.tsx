effcf141e53acdaa33b86da5f007028f36afdc9c
Emanuel Pecson <emanpecson@gmail.com>
2025-06-07 14:01:18 -0700

    fix: :bug: suppress hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <div className="min-h-screen w-screen overflow-x-hidden">
