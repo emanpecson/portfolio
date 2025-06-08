0d7e82f5c9cc4e834e28df3490adc2da932d794e
Emanuel Pecson <emanpecson@gmail.com>
2025-06-08 14:47:37 -0700

    style: :lipstick: use animated arrow for tasks w/ external links

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,18 +1,34 @@
+'use client';
+
 import { SkillType } from '@/types/SkillType';
 import IconLinkTag from './button/IconLinkTag';
-import BoxWrap from './BoxWrap';
 import { TaskType } from '@/types/TaskType';
 import Label from './Label';
+import { ArrowUpRightIcon } from 'lucide-react';
+import { useState } from 'react';
+import { cn } from '@/lib/utils';
 
 export default function TaskItem({ task }: { task: TaskType }) {
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
-    <div className="space-y-2">
+    <div className="space-y-2" onMouseOver={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
       <div>
         <div className="relative flex place-items-center">
           <div className="rounded-full bg-gray-600 dark:bg-neutral-300 h-1.5 w-1.5 absolute sm:-left-6 -left-4" />
           <div className="flex place-items-center space-x-2">
             <h3 className="font-bold dark:text-neutral-200 text-neutral-700">{task.header}</h3>
             {task.label && <Label>{task.label}</Label>}
+            {task.externalLink && (
+              <ArrowUpRightIcon
+                size={16}
+                strokeWidth={2.5}
+                className={cn(
+                  'text-neutral-600 dark:text-neutral-400 duration-100',
+                  isHovering && 'translate-x-0.5 -translate-y-0.5'
+                )}
+              />
+            )}
           </div>
         </div>
         <p className="text-neutral-600 dark:text-neutral-400">{task.description}</p>
