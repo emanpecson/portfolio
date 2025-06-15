ff17c911267baeacc2203dadf5ed9f2259f50aea
Emanuel Pecson <emanpecson@gmail.com>
2025-06-14 21:54:02 -0700

    style: :lipstick: slight re-style to under-development

--- src/components/UnderDevelopment.tsx
+++ src/components/UnderDevelopment.tsx
@@ -2,10 +2,11 @@ import { ConstructionIcon } from 'lucide-react';
 
 export default function UnderDevelopment() {
   return (
-    <div className="rounded-full px-4 py-1.5 bg-neutral-200 text-neutral-500 dark:bg-neutral-700 dark:text-neutral-300 w-fit border">
+    <div className="rounded-md px-3 py-1 bg-neutral-200 text-neutral-500 dark:bg-neutral-700 dark:text-neutral-300 w-fit border">
       <div className="flex space-x-2 place-items-center">
         <ConstructionIcon size={20} />
-        <span className="font-medium">Under development</span>
+        <span className="font-medium font-mono text-sm">Under development</span>
+        <ConstructionIcon size={20} />
       </div>
     </div>
   );
