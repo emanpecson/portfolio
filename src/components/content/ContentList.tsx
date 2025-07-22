5e8f1dfabe88a178d35a2ec3e46f188691cb6fb2
Emanuel Pecson <emanpecson@gmail.com>
2025-07-21 19:08:53 -0700

    style: :lipstick: prevent bullet pt from shrinking

--- src/components/content/ContentList.tsx
+++ src/components/content/ContentList.tsx
@@ -7,7 +7,7 @@ export default function ContentList(props: ContentListProps) {
     <ul className="font-light text-neutral-800 dark:text-neutral-200 flex flex-col space-y-1">
       {props.list.map((item, i) => (
         <li key={i} className="flex space-x-4 place-items-start pl-2">
-          <div className="rounded-full bg-neutral-800 dark:bg-neutral-200 w-1.5 h-1.5 mt-2" />
+          <div className="rounded-full bg-neutral-800 dark:bg-neutral-200 w-1.5 h-1.5 mt-2 flex-shrink-0" />
           <span>{item}</span>
         </li>
       ))}
