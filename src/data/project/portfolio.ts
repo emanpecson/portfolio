bad78dcc7e7da9f5f97763502ed3cd70dbccf534
Emanuel Pecson <emanpecson@gmail.com>
2025-08-01 21:59:57 -0700

    chore: :fire: hide portfolio project

--- src/data/project/portfolio.ts
+++
@@ -1,5 +0,0 @@
-// import { RefMap } from '@/lib/ref-map';
-import { ContentHeaderType } from '@/types/ContentHeader';
-
-// const refMap = new RefMap([]);
-export const headers: ContentHeaderType[] = [];
