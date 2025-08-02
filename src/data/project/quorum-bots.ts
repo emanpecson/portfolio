3c9a7ff286f500135c2a6de63059dd06171c2e72
Emanuel Pecson <emanpecson@gmail.com>
2025-08-01 21:57:51 -0700

    feat: :memo: add quorum-bots documentation

--- src/data/project/quorum-bots.ts
+++ src/data/project/quorum-bots.ts
@@ -1,5 +1,48 @@
-// import { RefMap } from '@/lib/ref-map';
+import { RefMap } from '@/lib/ref-map';
 import { ContentHeaderType } from '@/types/ContentHeader';
 
-// const refMap = new RefMap([]);
-export const headers: ContentHeaderType[] = [];
+const refMap = new RefMap([
+  '1(preview)',
+  '2(overview)',
+  '2.1(problem)',
+  '2.2(solution)',
+  '2.3(impact)',
+  '3(leadership-role)',
+  '3.1(contributor-guide)',
+]);
+
+export const headers: ContentHeaderType[] = [
+  {
+    id: 'Try it out',
+    ref: refMap.getRef('1(preview)'),
+    subheaders: [],
+  },
+  {
+    id: 'Overview',
+    ref: refMap.getRef('2(overview)'),
+    subheaders: [
+      {
+        id: 'Problem',
+        ref: refMap.getRef('2.1(problem)'),
+      },
+      {
+        id: 'Solution',
+        ref: refMap.getRef('2.2(solution)'),
+      },
+      {
+        id: 'Impact',
+        ref: refMap.getRef('2.3(impact)'),
+      },
+    ],
+  },
+  {
+    id: 'Leadership Role',
+    ref: refMap.getRef('3(leadership-role)'),
+    subheaders: [
+      {
+        id: 'Contributor guide',
+        ref: refMap.getRef('3.1(contributor-guide)'),
+      },
+    ],
+  },
+];
